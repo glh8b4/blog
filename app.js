ad56dd3f1df6ffa6a128797b9445f83c865298c2
Lou Harmon <glh8b4@mst.edu>
2014-11-13 20:23:45 -0600

    Submitting basic files

---
+++ app.js
@@ -0,0 +1,21 @@
+﻿(function(){
+	var app = angular.module('myApp', ['ngRoute'])
+	.config(['$routeProvider', function ($routeProvider) {
+		$routeProvider
+		  .when('/', {
+			  templateUrl: 'views/home.html',
+			  controller: 'homeController'
+		  })
+		  .when('/about', {
+			  templateUrl: 'views/about.html',
+			  controller: 'aboutController'
+		  })
+		  .otherwise({
+			  redirectTo: '/'
+		  });
+	}])
+	.controller('mainController', function ($scope) {
+		$scope.message = "Main Content";
+	});
+}());
+
---
+++ controllers/aboutController.js
@@ -0,0 +1,6 @@
+﻿(function() {
+	var app = angular.module('myApp', []);
+	app.controller('aboutController', function ($scope) {
+		$scope.message = "Now viewing about!";
+	});
+}());
